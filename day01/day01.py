97c8da5f669f8321b32ec507bdfe57f4c8e0192a
Poschi <development@poschi.net>
2020-12-01 23:03:29 +0100

    Day01 - B

--- day01/day01.py
+++ day01/day01.py
@@ -7,9 +7,18 @@ def find(expenses):
             if sum == 2020:
                 return expense * expense2
 
+def find2(expenses):
+    for expense in expenses:
+        for expense2 in expenses:
+            for expense3 in expenses:
+                sum = expense + expense2 + expense3
+                if sum == 2020:
+                    return expense * expense2 * expense3
+
 expenses = []
 with open('input.txt') as file:
     for expense in file.readlines():
         expenses.append(int(expense.strip()))
 
 print(str(find(expenses)))
+print(str(find2(expenses)))
