7477257d17c735f67ea163b8fb7e373757044c09
Poschi <development@poschi.net>
2020-12-01 23:01:26 +0100

    Day01 - A

---
+++ day01/day01.py
@@ -0,0 +1,15 @@
+import math
+
+def find(expenses):
+    for expense in expenses:
+        for expense2 in expenses:
+            sum = expense + expense2
+            if sum == 2020:
+                return expense * expense2
+
+expenses = []
+with open('input.txt') as file:
+    for expense in file.readlines():
+        expenses.append(int(expense.strip()))
+
+print(str(find(expenses)))
