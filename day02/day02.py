60b5021c776b55aa85347a42f16859c3b7a3e342
Poschi <development@poschi.net>
2020-12-02 21:00:01 +0100

    Day02 - B

--- day02/day02.py
+++ day02/day02.py
@@ -2,10 +2,10 @@
 
 import re
 
+# 5-8 j: jjjjjjjjjgjjjjjjjjj
 exp = re.compile(r'(\d+)-(\d+) (.): (.+)')
 
-# 5-8 j: jjjjjjjjjgjjjjjjjjj
-def check(policy):
+def check_a(policy):
     m = exp.match(policy)
     min = int(m.group(1))
     max = int(m.group(2))
@@ -18,10 +18,23 @@ def check(policy):
             count += 1
     return count >= min and count <= max
 
-count = 0
+def check_b(policy):
+    m = exp.match(policy)
+    position_a = int(m.group(1))
+    position_b = int(m.group(2))
+    pattern = m.group(3)
+    password = m.group(4)
+    
+    return (password[position_a-1] == pattern) ^ (password[position_b-1] == pattern)
+
+count_a = 0
+count_b = 0
 with open('input.txt') as file:
     for policy in file.readlines():
-        if(check(policy)):
-            count += 1
+        if(check_a(policy)):
+            count_a += 1
+        if(check_b(policy)):
+            count_b += 1
 
-print(str(count))
+print(str(count_a))
+print(str(count_b))
