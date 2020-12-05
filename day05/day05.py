b4a5a0b97ba202b7e7322554a37b8eeaf21042e1
Poschi <development@poschi.net>
2020-12-05 22:31:43 +0100

    Day05 - B

--- day05/day05.py
+++ day05/day05.py
@@ -7,12 +7,17 @@ def decode_column(column):
     return int(column.replace("L", "0").replace("R", "1"), 2)
 
 max = 0
+taken = {}
 with open('input.txt') as file:
     for boarding_pass in file.readlines():
-        boarding_pass = boarding_pass.strip()
         row = decode_row(boarding_pass[0:7])
-        column = decode_column(boarding_pass[-3:])
+        column = decode_column(boarding_pass[7:10])
         seat = row * 8 + column
+        taken[seat] = True
         if max < seat:
             max = seat
 print(max)
+
+for seat in range(1, max-1):
+    if seat not in taken and seat-1 in taken and seat+1 in taken:
+        print(seat)
