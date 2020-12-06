a50f6b5fc5dfd58bd75d617552323d43c6283355
Poschi <development@poschi.net>
2020-12-06 12:32:51 +0100

    Day06 - B

--- day06/day06.py
+++ day06/day06.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 
-def prccede_group(raw_group):
+def procede_group_or(raw_group):
     group = {}
     for raw_passenger in raw_group.split("\n"):
         for answer in raw_passenger.strip():
             group[answer] = True
     return len(group)
 
-sum = 0
+def procede_group_and(raw_group):
+    group = {}
+    for raw_passenger in raw_group.split("\n"):
+        for answer in raw_passenger.strip():
+            group[answer] = group[answer] + 1 if answer in group else 1
+    
+    passengers = raw_group.count("\n") + 1
+    yeses = 0
+    for answers in group.values():
+        if answers >= passengers:
+            yeses += 1
+    return yeses
+
+sum_or = 0
+sum_and = 0
 with open('input.txt') as file:
     raw_data = file.read()
     for raw_group in raw_data.split("\n\n"):
-        sum += prccede_group(raw_group)
+        sum_or += procede_group_or(raw_group.strip())
+        sum_and += procede_group_and(raw_group.strip())
        
-print(sum)
+print(sum_or)
+print(sum_and)
