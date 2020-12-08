73004829c065000e8e264a976f6fa61af24e86ad
Poschi <development@poschi.net>
2020-12-08 23:28:09 +0100

    Day08 - B

--- day08/day08.py
+++ day08/day08.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import copy
+
 class Instruction:
     def __init__(self, input):
         parsed = input.strip().split(" ")
@@ -10,28 +12,52 @@ class Instruction:
     def __repr__(self):
         return self.type + ": " + str(self.number)
 
-instructions = []
+class Run:
+    def __init__(self, instructions, endAtEnd):
+        self.acc = 0
+        self.instruction_number = 0
+        self.instructions = instructions
+        self.endAtEnd = endAtEnd
+    
+    def done(self, instruction):
+        if self.endAtEnd and self.instruction_number == len(self.instructions)-1:
+            return True
+        return instruction.done is True
+
+    def run(self):
+        instruction = self.instructions[self.instruction_number]
+        while not self.done(instruction):
+            if instruction.type == "nop":
+                self.instruction_number += 1
+            elif instruction.type == "acc":
+                self.acc += instruction.number
+                self.instruction_number += 1
+            elif instruction.type == "jmp":
+                self.instruction_number += instruction.number
+            else:
+                raise Exception()
+            
+            instruction.done = True
+            instruction = self.instructions[self.instruction_number]
+        return self.acc, self.instruction_number
+
+original_instructions = []
 with open('input.txt') as file:
     for command in file.readlines():
         inst = Instruction(command)
-        instructions.append(inst)
+        original_instructions.append(inst)
 
-acc = 0
-instruction_number = 0
+print(Run(copy.deepcopy(original_instructions), False).run())
 
-instruction = instructions[instruction_number]
-while instruction.done is False:
+for i in range(len(original_instructions)):
+    instruction = original_instructions[i]
+    if instruction.type == "acc":
+        continue
+    tmp_instructions = copy.deepcopy(original_instructions)
     if instruction.type == "nop":
-        instruction_number += 1
-    elif instruction.type == "acc":
-        acc += instruction.number
-        instruction_number += 1
+        tmp_instructions[i].type = "jmp"
     elif instruction.type == "jmp":
-        instruction_number += instruction.number
-    else:
-        raise Exception()
-    
-    instruction.done = True
-    instruction = instructions[instruction_number]
-
-print(acc)
+        tmp_instructions[i].type = "nop"
+    res = Run(tmp_instructions, True).run()
+    if res[1] == len(original_instructions) -1:
+        print(res)
